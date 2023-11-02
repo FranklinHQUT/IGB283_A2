0b58fd0fa79299125870513a90c9cbe5300531b7
Connors107 <connorstiles107@gmail.com>
2023-11-03 03:11:12 +1000

    finished building simple shape
    his name is vincent, he is my beautiful son :)

---
+++ Assessment_2/Assets/IGB283Transform.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IGB283Transform
+{
+    public static Matrix3x3 Rotate(float angle)
+    {
+        // Create a new matrix
+        Matrix3x3 matrix = new Matrix3x3();
+        // Set the rows of the matrix
+        matrix.SetRow(0, new Vector3(Mathf.Cos(angle), -Mathf.Sin(angle), 0.0f));
+        matrix.SetRow(1, new Vector3(Mathf.Sin(angle), Mathf.Cos(angle), 0.0f));
+        matrix.SetRow(2, new Vector3(0.0f, 0.0f, 1.0f));
+        // Return the matrix
+        return matrix;
+    }
+
+    public static Matrix3x3 Translate(Vector3 offset)
+    {
+        // Create a new matrix
+        Matrix3x3 matrix = new Matrix3x3();
+        // Set the rows of the matrix
+        matrix.SetRow(0, new Vector3(1.0f, 0.0f, offset.x));
+        matrix.SetRow(1, new Vector3(0.0f, 1.0f, offset.y));
+        matrix.SetRow(2, new Vector3(0.0f, 0.0f, 1.0f));
+        // Return the matrix
+        return matrix;
+    }
+
+    public static Matrix3x3 Scale(float scale)
+	{
+		Matrix3x3 matrix = new Matrix3x3();
+
+		//scaledMatrix = [scalex 0, 0 scaley]
+		matrix.SetRow(0, new Vector3(scale, 0.0f, 0.0f));
+		matrix.SetRow(1, new Vector3(0.0f, scale, 0.0f));
+		matrix.SetRow(2, new Vector3(0.0f, 0.0f, 1.0f));
+
+		return matrix;
+	}
+
+}
